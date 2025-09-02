b03077ab6d5e6b89a57c53abe67aca893cc88fc8
Maxomiliano <max.nebula1@gmail.com>
2025-09-02 20:59:39 -0300

    Board data passable condition

--- ProgVJ2/Assets/Scripts/BoardManager.cs
+++ ProgVJ2/Assets/Scripts/BoardManager.cs
@@ -3,6 +3,12 @@ using UnityEngine.Tilemaps;
 
 public class BoardManager : MonoBehaviour
 {
+    public class CellData 
+    {
+        public bool Passable;
+    }
+
+    private CellData[,] _boardData;
     private Tilemap _tileMap;
 
     public int Width;
@@ -14,19 +20,24 @@ public class BoardManager : MonoBehaviour
     void Start()
     {
         _tileMap = GetComponentInChildren<Tilemap>();
+        _boardData = new CellData[Width, Height];
 
         for (int y = 0; y < Height; y++)
         {
             for (int x = 0; x < Width; x++)
             {
                 Tile tile;
+                _boardData[x, y] = new CellData();
+
                 if (x == 0 || y == 0 || x == Width - 1 || y == Height - 1)
                 {
                     tile = WallTiles[Random.Range(0, WallTiles.Length)];
+                    _boardData[x, y].Passable = false;
                 }
                 else
                 {
                     tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
+                    _boardData[x, y].Passable = true;
                 }
 
                 _tileMap.SetTile(new Vector3Int(x, y, 0), tile);
