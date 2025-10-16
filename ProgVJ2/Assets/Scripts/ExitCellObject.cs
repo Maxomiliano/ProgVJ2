4255d37683a027b1f4a40fc9c5ee94ceae003807
Maxomiliano <max.nebula1@gmail.com>
2025-10-16 15:54:18 -0300

    food and exit sounds

--- ProgVJ2/Assets/Scripts/ExitCellObject.cs
+++ ProgVJ2/Assets/Scripts/ExitCellObject.cs
@@ -3,18 +3,22 @@ using UnityEngine.Tilemaps;
 
 public class ExitCellObject : CellObject
 {
+    [SerializeField] private AudioClip _exitClip;
+
     public Tile EndTile;
     public int experienceOnExit = 20;
+    private Vector3 _soundClipPos;
 
     public override void Init(Vector2Int coord)
     {
         base.Init(coord);
-
+        _soundClipPos = new Vector3(4, 4, -10);
         GameManager.Instance.BoardManager.SetCellTile(coord, EndTile);
     }
 
     public override void PlayerEntered()
     {
+        AudioSource.PlayClipAtPoint(_exitClip, _soundClipPos);
         PlayerController player = GameManager.Instance.PlayerController;
         PlayerProgression progression = player.GetComponent<PlayerProgression>();
         if (progression != null)
--- ProgVJ2/Assets/Scripts/FoodObject.cs
+++ ProgVJ2/Assets/Scripts/FoodObject.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class FoodObject : CellObject
 {
+    [SerializeField] private AudioClip _foodSound;
+
+    private Vector3 _soundClipPos;
     public int AmountGranted = 10;
+
+    private void Start()
+    {
+        _soundClipPos = new Vector3(4, 4, -10);
+    }
 
     public override void PlayerEntered()
     {
+        AudioSource.PlayClipAtPoint(_foodSound, _soundClipPos);
         Destroy(gameObject);
         GameManager.Instance.ChangeFood(AmountGranted);
     }
